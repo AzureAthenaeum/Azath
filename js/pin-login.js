37ff582613ef6f6ab94d22f16fa4bcd22a165c68
AzureAthenaeum <73016365+AzureAthenaeum@users.noreply.github.com>
2020-10-28 15:50:04 -0500

    up

--- js/pin-login.js
+++ js/pin-login.js
@@ -16,13 +16,14 @@ class PinLogin {
 
     _generatePad() {
 
-      const logo = document.querySelector(".logo")
+      const logo = document.querySelector(".logo");
+
 
         const padLayout = [
             "1", "2", "3",
             "4", "5", "6",
             "7", "8", "9",
-            "backspace", "0", "done"
+            "backspace", "0", "cloud_done"
         ];
 
         padLayout.forEach(key => {
@@ -46,7 +47,7 @@ class PinLogin {
             case "backspace":
                 this.value = this.value.substring(0, this.value.length - 1);
                 break;
-            case "done":
+            case "cloud_done":
                 this._attemptLogin();
                 break;
             default:
@@ -69,7 +70,7 @@ class PinLogin {
             fetch(this.loginEndpoint, {
                 method: "post",
                 headers: {
-                    "Content-Type": "application/x-www-form-urlencoded"
+                    "Content-Type": "multipart/form-data"
                 },
                 body: `pincode=${this.value}`
             }).then(response => {
